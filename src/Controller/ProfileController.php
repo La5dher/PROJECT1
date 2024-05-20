f4375d98c69ce035ac9294742500bc54e85133e0
Ilyes Aljane <aljaneelyes07@gmail.com>
2024-05-20 18:17:36 +0100

    profile added

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'app_profile')]
+    public function index(UserInterface $user = null): Response
+    {
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('profile/index.html.twig', [
+            'user' => $user,
+        ]);
+        // return $this->render('profile/index.html.twig', [
+        //     'controller_name' => 'ProfileController',
+        // ]);
+    }
+}
